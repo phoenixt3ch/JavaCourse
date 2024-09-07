5456a55013b031a9d4c3b9e87790b50744ffc0e8
phoenixt3ch <kocil123@gmail.com>
2024-09-07 13:22:58 +0300

    init

---
+++ src/main/java/ru/vsu/cs/kostryukov/Calculator.java
@@ -0,0 +1,5 @@
+package ru.vsu.cs.kostryukov;
+
+public class Calculator implements ICalculator {
+
+}
---
+++ src/main/java/ru/vsu/cs/kostryukov/Main.java
@@ -0,0 +1,7 @@
+package ru.vsu.cs.kostryukov;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(ICalculator.getFour());
+    }
+}
